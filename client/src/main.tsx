eccd3ffeee1aa9eb6214cdb823c0e26eb7ff727a
yura4ka <yura4ka05@gmail.com>
2024-01-26 17:07:15 +0200

    feat: add theme switcher

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
 import "./lib/i18n";
+import { ThemeProvider } from "@/components/theme/ThemeProvider.tsx";
+import { store } from "@/app/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
 );
