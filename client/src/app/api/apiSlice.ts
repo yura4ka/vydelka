e48482e7da21d36c5892c8a9c70d87eb64c05ad5
yura4ka <yura4ka05@gmail.com>
2024-01-30 23:56:19 +0200

    feat: add category api slice

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -33,5 +33,5 @@ const baseQueryWithAuth: typeof baseQuery = async (args, api, endpoints) => {
 export const api = createApi({
   baseQuery: baseQueryWithAuth,
   endpoints: () => ({}),
-  tagTypes: ["Category"],
+  tagTypes: ["CategoryInfo", "CategoryList"],
 });
---
+++ client/src/features/categories/categoriesApiSlice.ts
@@ -0,0 +1,103 @@
+import { api } from "@/app/api/apiSlice";
+import { Language, Translation } from "../translations";
+import i18next from "i18next";
+
+export type Category = {
+  id: string;
+  createdAt: string;
+  title: string;
+  slug: string;
+  parentId?: string;
+  imageUrl: string;
+};
+
+export type NewCategory = {
+  title: Translation;
+  slug: string;
+  parentId?: string;
+  imageUrl: string;
+};
+
+export type CategoryInfo = Category & {
+  title: Translation;
+};
+
+export type ChangeCategory = {
+  id: string;
+} & NewCategory;
+
+export const categoryApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getCategories: builder.query<Category[], string | void>({
+      query: (id) => ({ url: id ? `category?parent_id=${id}` : `category` }),
+      providesTags: (result, _, id) =>
+        result
+          ? [
+              ...result.map((c) => ({
+                type: "CategoryList" as const,
+                id: c.id,
+                categoryId: id,
+              })),
+              { type: "CategoryList", categoryId: id },
+            ]
+          : [{ type: "CategoryList", categoryId: id }],
+    }),
+
+    createCategory: builder.mutation<{ id: string }, NewCategory>({
+      query: (body) => ({ url: `category`, method: "POST", body }),
+      onQueryStarted: async (category, { dispatch, queryFulfilled }) => {
+        const lang = i18next.resolvedLanguage as Language;
+        const result = await queryFulfilled;
+        dispatch(
+          categoryApi.util.updateQueryData(
+            "getCategories",
+            category.parentId,
+            (draft) => {
+              draft.splice(0, 0, {
+                ...category,
+                id: result.data.id,
+                title: category.title[lang],
+                createdAt: new Date().toString(),
+              });
+            },
+          ),
+        );
+      },
+    }),
+
+    getCategoryById: builder.query<CategoryInfo, string>({
+      query: (id) => ({ url: `category/${id}` }),
+      providesTags: (_result, _err, id) => [{ type: "CategoryInfo", id }],
+    }),
+
+    changeCategory: builder.mutation<undefined, ChangeCategory>({
+      query: (body) => ({ url: `category`, method: "PUT", body }),
+      invalidatesTags: (_result, _error, category) => [
+        { type: "CategoryList", id: category.id },
+      ],
+      onQueryStarted: async (category, { dispatch, queryFulfilled }) => {
+        const patchResult = dispatch(
+          categoryApi.util.updateQueryData(
+            "getCategoryById",
+            category.id,
+            (draft) => {
+              Object.assign(draft, category);
+            },
+          ),
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetCategoriesQuery,
+  useCreateCategoryMutation,
+  useChangeCategoryMutation,
+  useGetCategoryByIdQuery,
+} = categoryApi;
