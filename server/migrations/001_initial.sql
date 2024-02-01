2b7a5f3a36f33d1e2c2489bee1f31d2c2f3ea47a
yura4ka <yura4ka05@gmail.com>
2024-02-01 19:48:33 +0200

    feat: add reference on delete statements

--- server/migrations/001_initial.sql
+++ server/migrations/001_initial.sql
@@ -28,7 +28,7 @@ CREATE TABLE translation_items (
 );
 
 CREATE TABLE translations (
-  item_id UUID NOT NULL REFERENCES translation_items(id),
+  item_id UUID NOT NULL REFERENCES translation_items(id) ON DELETE CASCADE,
   lang language_type NOT NULL,
   content TEXT NOT NULL,
   PRIMARY KEY (item_id, lang)
@@ -37,9 +37,9 @@ CREATE TABLE translations (
 CREATE TABLE categories (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  title_translation_item UUID NOT NULL REFERENCES translation_items(id),
+  title_translation_item UUID NOT NULL REFERENCES translation_items(id) ON DELETE CASCADE,
   slug VARCHAR(64) NOT NULL UNIQUE,
-  parent_id UUID REFERENCES categories(id),
+  parent_id UUID REFERENCES categories(id) ON DELETE SET NULL,
   image_url TEXT NOT NULL
 );
 
@@ -65,23 +65,23 @@ CREATE INDEX product_tsv_idx ON product_translations USING GIN (search);
 
 CREATE TABLE filters (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  title_translation_item UUID NOT NULL REFERENCES translation_items(id),
+  title_translation_item UUID NOT NULL REFERENCES translation_items(id) ON DELETE CASCADE,
   slug VARCHAR(64) NOT NULL,
-  category_id UUID NOT NULL REFERENCES categories(id),
+  category_id UUID NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
   UNIQUE(slug, category_id)
 );
 
 CREATE TABLE filter_variants (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   slug VARCHAR(64) NOT NULL,
-  variant_translation_item UUID NOT NULL REFERENCES translation_items(id),
-  filter_id UUID NOT NULL REFERENCES filters(id),
+  variant_translation_item UUID NOT NULL REFERENCES translation_items(id) ON DELETE CASCADE,
+  filter_id UUID NOT NULL REFERENCES filters(id) ON DELETE CASCADE,
   UNIQUE(slug, filter_id)
 );
 
 CREATE TABLE product_filters (
-  product_id UUID NOT NULL REFERENCES products(id),
-  variant_id UUID NOT NULL REFERENCES filter_variants(id),
+  product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+  variant_id UUID NOT NULL REFERENCES filter_variants(id) ON DELETE CASCADE,
   PRIMARY KEY (product_id, variant_id)
 );
 
@@ -112,7 +112,7 @@ CREATE TABLE orders (
   pay pay_type NOT NULL,
   payment_time TIMESTAMPTZ,
   region TEXT,
-  user_id UUID NOT NULL REFERENCES users(id)
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE INDEX idx_orders_user ON orders (user_id);
@@ -124,8 +124,8 @@ CHECK (
 );
 
 CREATE TABLE order_content (
-  order_id UUID NOT NULL REFERENCES orders(id),
-  product_id UUID NOT NULL REFERENCES products(id),
+  order_id UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+  product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
   quantity INT NOT NULL CHECK(quantity > 0),
   PRIMARY KEY (order_id, product_id)
 );
@@ -136,8 +136,8 @@ CREATE TABLE reviews (
   updated_at TIMESTAMPTZ,
   content TEXT NOT NULL CHECK(LENGTH(content) <= 10000),
   rating SMALLINT NOT NULL CHECK(rating >= 0 AND rating <= 5),
-  user_id UUID NOT NULL REFERENCES users(id),
-  product_id UUID NOT NULL REFERENCES products(id)
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE
 );
 
 CREATE TRIGGER set_timestamp
