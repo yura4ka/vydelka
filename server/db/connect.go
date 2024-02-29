2101363fafecc3d4cf49f6dac90f372b2a20697d
yura4ka <yura4ka05@gmail.com>
2024-03-01 01:09:48 +0200

    feat: add initial admin user setup

--- server/db/connect.go
+++ server/db/connect.go
@@ -9,10 +9,27 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var Client *pgxpool.Pool
 var Ctx context.Context
+
+func addAdmin() error {
+	log.Println("creating admin user...")
+	hashed, err := bcrypt.GenerateFromPassword([]byte(os.Getenv("POSTGRES_PASSWORD")), 10)
+	if err != nil {
+		return err
+	}
+
+	_, err = Client.Exec(Ctx, `
+		INSERT INTO users (first_name, last_name, phone, is_admin, email, password)
+		VALUES ('Admin', 'Admin', '+380950000000', TRUE, $1, $2)
+		ON CONFLICT DO NOTHING;
+	`, os.Getenv("EMAIL_FROM"), hashed)
+
+	return err
+}
 
 func Connect(migrations embed.FS) {
 	log.Println("connecting to database...")
@@ -30,6 +47,10 @@ func Connect(migrations embed.FS) {
 	}
 
 	if err := goose.Up(stdlib.OpenDBFromPool(Client), "migrations"); err != nil {
+		panic(err)
+	}
+
+	if err := addAdmin(); err != nil {
 		panic(err)
 	}
 }
